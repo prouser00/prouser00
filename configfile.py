99b9d7e34d771c4f120f28b799b74d9ea08baa90
prouser00 <96454760+prouser00@users.noreply.github.com>
2021-12-20 21:53:51 -0400

    Add files via upload

---
+++ configfile.py
@@ -0,0 +1,53 @@
+import sys
+
+
+##Newly added variables v2.6
+buytimes='1'
+enablestoploss='0'
+enabletokenbuy='0'
+ttokenbuy='0'
+sstoploss='0'
+##
+
+token_address='0x566b7842f73aaef3c0ac981765a47708bdcb0137'
+decimals='18'
+bnb_amount='0.000534063'
+speedms='0'
+sec_wait='1'
+max_slippage='2'
+gwei='20'
+infura_url='https://bsc-dataseed.binance.org/'
+addedurl=''
+enableprofitsell='0'
+pprofitsell='1'
+
+enable_waiting='0'
+liq_before='0'
+approvetoken='0'
+
+enable_pricebuying='0'
+enable_priceselling='0'
+enablesellslippage='0'
+onlysell='1'
+buytimes='1'
+enablestoploss ='0'
+enabletokenbuy ='0'
+tokenbuy='0'
+
+gasusage='700000'
+sellslippage='40'
+pricebuy='2'
+pricesell='0.5'
+selltimesenable='0'
+selltimes='1'
+version='2'
+
+
+retrywhenerror='0'
+amount_threads='1'
+getclipboard='1'
+my_address = '0x5f607E94b37748Aa081bc96703aC5BA8e84D5c34'
+my_pk = '4bceb1453c7c061e4874268517b3db057dc72cb6f8a3ff756e9cc89b5821f5bc'
+
+
+sys.path.append(".")
